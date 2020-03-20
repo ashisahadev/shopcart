94e8e0995ec55be07b0ad35730b6ba41cdf50a6a
ashisahadev <30519133+ashisahadev@users.noreply.github.com>
2020-03-21 02:51:21 +0530

    Update database.php

--- api/database.php
+++ api/database.php
@@ -3,10 +3,10 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: PUT, GET, POST, DELETE");
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 
- define('DB_HOST', 'localhost');
- define('DB_USER', 'root');
- define('DB_PASS', '');
- define('DB_NAME', 'shopcart');
+ define('DB_HOST', 'h40lg7qyub2umdvb.cbetxkdyhwsb.us-east-1.rds.amazonaws.com');
+ define('DB_USER', 'k0xn1m4pw6ryzh4i');
+ define('DB_PASS', 'znrdxcigwasq31jk');
+ define('DB_NAME', 'r18fzehqvyfcnw3p');
 function connect()
 {
   $connect = mysqli_connect(DB_HOST ,DB_USER ,DB_PASS ,DB_NAME);
